4325ff77a2744fcad7e0ce2ce8f0e723fea99ef0
AtsushiHosaka <hosaka2917@gmail.com>
2022-11-21 13:17:20 +0900

    Initial Commit

---
+++ iTunesAPI_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesAPI_Demo
+//
+//  Created by 保坂篤志 on 2022/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iTunesAPI_DemoUITests/iTunesAPI_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iTunesAPI_DemoUITestsLaunchTests.swift
+//  iTunesAPI_DemoUITests
+//
+//  Created by 保坂篤志 on 2022/11/21.
+//
+
+import XCTest
+
+final class iTunesAPI_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
